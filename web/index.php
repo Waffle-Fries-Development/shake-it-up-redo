90f29eab262cd45a7f579b1a54273c08afa7ba73
Hayden King <hayden.king@gmail.com>
2017-10-18 23:58:01 -0500

    first commit.

---
+++ web/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp/wp-blog-header.php';
